436aabe61dc45a9937df49d79acf1cbb507b3f92
Elliott-fire <redoakfire110@yahoo.com>
2026-07-29 11:18:45 -0500

    Use Command Center header in shared layout
    Updated PageLayout to use the Command Center header. Verified Apparatus and Command Center use the same app shell.

--- components/layout/PageLayout.tsx
+++ components/layout/PageLayout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import Sidebar from "@/components/command-center-v3/Sidebar";
-import TopBar from "./TopBar";
+import Header from "@/components/command-center-v3/Header";
 
 interface PageLayoutProps {
   children: ReactNode;
@@ -15,7 +15,7 @@ export default function PageLayout({
         <Sidebar />
 
         <div className="flex min-w-0 flex-1 flex-col">
-          <TopBar />
+          <Header />
 
           <main className="flex-1 overflow-y-auto bg-neutral-950">
             <div className="w-full px-4 py-4 lg:px-6 xl:px-8">
